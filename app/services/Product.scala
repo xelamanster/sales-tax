c5e3cab53ac989dac297765d970a1a04bafaa387
xelamanster <katerynaosypova99@gmail.com>
2016-11-24 09:47:13 +0200

    Added initial implementation of the tax.

---
+++ app/services/Product.scala
@@ -0,0 +1,41 @@
+package services
+
+trait Product {
+  val description: String
+  val count: Int
+  val unitPrice: Double
+}
+
+trait ExtendedProduct extends Product {
+  val exempt: Boolean
+  val imported: Boolean
+}
+
+trait Tax {
+  self: Product =>
+  def value: Double
+
+  def fullPrice = unitPrice + unitPrice * value
+}
+
+trait BaseTax extends Tax {
+  self: Product =>
+  override def value: Double = 0.1
+}
+
+trait FoodTax extends Tax {
+  self: Product =>
+  override def value: Double = 0.05
+}
+
+trait ImportedTax extends Tax {
+  self: Product =>
+  abstract override def value: Double = super.value + 0.1
+}
+
+class MyProduct(val description: String, val count: Int, val unitPrice: Double) extends Product
+
+object Test extends App {
+  val test = new MyProduct(" " , 1, 1) with FoodTax with ImportedTax
+  println(test.fullPrice)
+}
---
+++ app/utils/ProductsParser.scala
@@ -0,0 +1,6 @@
+package utils
+
+
+class ProductsParser {
+
+}
