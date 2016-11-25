9b7e77cbc7937e68037e31119342845a9e8ddcd7
xelamanster <xelamanster@gmail.com>
2016-11-25 13:40:03 +0200

    Reformatted test json.

--- test/data/TestData.scala
+++ test/data/TestData.scala
@@ -18,7 +18,10 @@ object TestData {
       }
     """
 
-  val validJsonOrder = """[{"description":"Book","count":1,"unitPrice":12.49},{"description":"Chocolate Bar","count":1,"unitPrice":0.85},{"description":"Music CD","count":1,"unitPrice":14.99}]"""
+  val validJsonOrder =
+    """[{"description":"Book","count":1,"unitPrice":12.49},""" +
+    """{"description":"Chocolate Bar","count":1,"unitPrice":0.85},""" +
+    """{"description":"Music CD","count":1,"unitPrice":14.99}]"""
 
   val parsedJsonOrder = (Seq(
     OrderItem("Book", 1, 12.49),
